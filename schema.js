f17fc32127b9596e62bb0bc545f6052b4f205098
Iftekhar16 <iftekharhossain16@gmail.com>
2024-10-07 20:56:47 +0600

    made changes to schema

--- schema.js
+++ schema.js
@@ -3,33 +3,33 @@ export const typeDefs = `#graphql
     id: ID!
     name: String!
     description: String!
-    price: Float!
+    price: Float
     discount: Int
     discountedPrice: Float
     tags: [String!]
     wishlist: Boolean!
     developer: String!
     publisher: String
-    releaseDate: String!
-    rating: Float!
-    platform: [String!]!
-    images: images!
+    releaseDate: String
+    rating: Float
+    platform: [String!]
+    images: images
   }
 
   type images {
-    logo: String!
-    bannerLandscape: String!
-    bannerPortrait: String!
-    bannerSquare: String!
-    screenshot: screenshot!
+    logo: String
+    bannerLandscape: String
+    bannerPortrait: String
+    bannerSquare: String
+    screenshot: screenshot
   }
 
   type screenshot {
-    screenshot1: String!
-    screenshot2: String!
-    screenshot3: String!
-    screenshot4: String!
-    screenshot5: String!
+    screenshot1: String
+    screenshot2: String
+    screenshot3: String
+    screenshot4: String
+    screenshot5: String
   }
 
   type Query{
